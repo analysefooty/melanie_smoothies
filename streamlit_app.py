490fe3313b64a4a78145bf60dd43d5cf8a2eb1b7
analysefooty <analysefootball@gmail.com>
2024-08-02 09:12:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 #session = get_active_session()
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
-pd_df = my_dataframe.to_panda()
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe,use_container_width=True)
 
 name_on_order = st.text_input('Name on the Order:')
